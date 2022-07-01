e9261101d0d92693b9d54017380869c3ab93e7b3
Luigi <gigitux@gmail.com>
2022-07-01 15:47:22 +0200

    retry test

--- tests/e2e/config/jest.setup.js
+++ tests/e2e/config/jest.setup.js
@@ -11,6 +11,7 @@ import {
 	visitAdminPage,
 } from '@wordpress/e2e-test-utils';
 import { setDefaultOptions } from 'expect-puppeteer';
+import { get } from 'lodash';
 /**
  * Internal dependencies
  */
@@ -39,6 +40,8 @@ const OBSERVED_CONSOLE_MESSAGE_TYPES = {
 async function setupBrowser() {
 	await setBrowserViewport( 'large' );
 }
+
+jest.retryTimes( 2 );
 
 /**
  * Navigates to WooCommerce's import page and imports sample products.
@@ -110,12 +113,33 @@ function observeConsoleLogging() {
 		if ( ! OBSERVED_CONSOLE_MESSAGE_TYPES.hasOwnProperty( type ) ) {
 			return;
 		}
-		const text = message.text();
+
+		let text = message.text();
+
+		// An exception is made for _blanket_ deprecation warnings: Those
+		// which log regardless of whether a deprecated feature is in use.
+		if ( text.includes( 'This is a global warning' ) ) {
+			return;
+		}
+
+		// A chrome advisory warning about SameSite cookies is informational
+		// about future changes, tracked separately for improvement in core.
+		//
+		// See: https://core.trac.wordpress.org/ticket/37000
+		// See: https://www.chromestatus.com/feature/5088147346030592
+		// See: https://www.chromestatus.com/feature/5633521622188032
+		if (
+			text.includes( 'A cookie associated with a cross-site resource' )
+		) {
+			return;
+		}
+
 		// Viewing posts on the front end can result in this error, which
 		// has nothing to do with Gutenberg.
 		if ( text.includes( 'net::ERR_UNKNOWN_URL_SCHEME' ) ) {
 			return;
 		}
+
 		// Network errors are ignored only if we are intentionally testing
 		// offline mode.
 		if (
@@ -124,10 +148,44 @@ function observeConsoleLogging() {
 		) {
 			return;
 		}
+
+		// As of WordPress 5.3.2 in Chrome 79, navigating to the block editor
+		// (Posts > Add New) will display a console warning about
+		// non - unique IDs.
+		// See: https://core.trac.wordpress.org/ticket/23165
+		if ( text.includes( 'elements with non-unique id #_wpnonce' ) ) {
+			return;
+		}
+
+		// Ignore all JQMIGRATE (jQuery migrate) deprecation warnings.
+		if ( text.includes( 'JQMIGRATE' ) ) {
+			return;
+		}
+
 		const logFunction = OBSERVED_CONSOLE_MESSAGE_TYPES[ type ];
-		// Disable reason: We intentionally bubble up console error messages
-		// for debugging reasons. If you need to test explicitly the logging,
-		// use  @wordpress/jest-console
+
+		// As of Puppeteer 1.6.1, `message.text()` wrongly returns an object of
+		// type JSHandle for error logging, instead of the expected string.
+		//
+		// See: https://github.com/GoogleChrome/puppeteer/issues/3397
+		//
+		// The recommendation there to asynchronously resolve the error value
+		// upon a console event may be prone to a race condition with the test
+		// completion, leaving a possibility of an error not being surfaced
+		// correctly. Instead, the logic here synchronously inspects the
+		// internal object shape of the JSHandle to find the error text. If it
+		// cannot be found, the default text value is used instead.
+		text = get(
+			message.args(),
+			[ 0, '_remoteObject', 'description' ],
+			text
+		);
+
+		// Disable reason: We intentionally bubble up the console message
+		// which, unless the test explicitly anticipates the logging via
+		// @wordpress/jest-console matchers, will cause the intended test
+		// failure.
+
 		// eslint-disable-next-line no-console
 		console[ logFunction ]( text );
 	} );
@@ -139,7 +197,7 @@ function observeConsoleLogging() {
 beforeAll( async () => {
 	capturePageEventsForTearDown();
 	enablePageDialogAccept();
-	// observeConsoleLogging();
+	observeConsoleLogging();
 	await setupBrowser();
 	await importSampleProducts();
 } );
